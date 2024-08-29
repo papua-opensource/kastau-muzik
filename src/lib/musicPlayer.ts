02a6d03fdfc92ad2b839fb54cb632b53df5d037e
antheiz <salupacode@gmail.com>
2024-08-29 10:44:40 +0700

    fix: update loader for rethym on hero

--- src/lib/musicPlayer.ts
+++ src/lib/musicPlayer.ts
@@ -52,6 +52,7 @@ function updatePlayState(isPlaying: boolean): void {
         musicControlPlayPauseButton.querySelector('.play-icon')?.classList.add('hidden');
         musicControlPlayPauseButton.querySelector('.pause-icon')?.classList.remove('hidden');
         heroPlayingStatus.classList.remove('hidden');
+        heroPlayingStatus.classList.add('inline-flex');
     } else {
         heroPlayPauseButton.innerHTML = '<svg class="w-6" fill="currentColor" viewBox="0 0 24 24"><path d="M16.53,11.152l-8-5C8.221,5.958,7.833,5.949,7.515,6.125C7.197,6.302,7,6.636,7,7v10 c0,0.364,0.197,0.698,0.515,0.875C7.667,17.958,7.833,18,8,18c0.184,0,0.368-0.051,0.53-0.152l8-5C16.822,12.665,17,12.345,17,12 S16.822,11.335,16.53,11.152z"></path></svg>';
         musicControlPlayPauseButton.querySelector('.play-icon')?.classList.remove('hidden');
